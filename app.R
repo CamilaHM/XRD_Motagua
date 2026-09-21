5df3d98b29529d717ccc96365dfaab22c4462731
CamilaHM <59892445+CamilaHM@users.noreply.github.com>
2026-09-21 17:13:10 +0200

    Update app.R

--- app.R
+++ app.R
@@ -37,11 +37,11 @@ datos_mapa <- datos_mapa %>%
 datos_finales <- left_join(dataSitios, datos_mapa, by = "site")
 unique(datos_finales$site)
 
-frecuencia_minerales <- colSums(datos_finales[cols_minerales], na.rm = TRUE)
+cols_used <- setdiff(names(datos_finales), c("site", "lat", "long"))
+
+frecuencia_minerales <- colSums(datos_finales[cols_used], na.rm = TRUE)
 
 cols_minerales <- names(sort(frecuencia_minerales, decreasing = TRUE))
-
-
 # Paleta de colores
 palette_completa <- setNames(
     colorRampPalette(c("#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf", "#aec7e8", "#ffbb78", "#98df8a", "#ff9896", "#c5b0d5", "#c49c94", "#f7b6d2", "#c7c7c7", "#9edae5", "#393b79"))(length(cols_minerales)),
